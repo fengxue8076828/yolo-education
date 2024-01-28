692f2c1942cc6a14f8b9d5d11306c5061a912cf3
York <york@infuture.com>
2024-01-28 23:41:39 +0100

    changed sanity account

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -4,10 +4,10 @@ import { apiVersion, dataset, projectId, useCdn } from '../env'
 
 export const client = createClient({
   apiVersion:"2024-01-05",
-  dataset:"yolo-education",
-  projectId:"hq8ftxuw",
+  dataset,
+  projectId,
   useCdn:true,
-  token:"skapjz9dSlefaTPx2ejYc5WwQzWOxjvLT4mAmvraFMgq3XQPpJ1eD6fVQQLcalifI8PWaImYFNz8Sy88MWU8oweq2ZtO3zsZvwNR1zjaUltIvVZwbzOOOJ9ubNC4Q3d2TkEPpps39Ca0IMDev5e3t7NROLKAWHqhdKKd8WjpnAHWFYxeYrc4"
+  token:"skVikxp1Og5y1PZq7e3Sq7bkQqVuDFsyrnOQkScKO4RvnrkU5wbsh1W2gHY91sDQw3yzdUCvzyOBTyLFwgsRZs0Kl4AOZx78SKcJiUaA1Chw1qRcIBr23YqdlRPrHb3ZOgxE1bH7Dmtft21bUdJdgzhDb8c4ss5Y8sIoGafyku0PLdCxcLfl"
 })
 
 const imageBuilder = imageUrlBuilder(client)
